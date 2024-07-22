7b86041fd77c43fd96b9574eb4c0efe3dbe6e647
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-22 14:59:11 -0400

    add thingy for popover

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -31,20 +31,24 @@ const TextInput = ({
   };
 
   return (
-    <textarea
-      value={value}
-      onChange={handleChange}
-      onKeyDown={handleKeyDown}
-      disabled={disabled}
-      placeholder={placeholder}
-      className={`border-2 bg-black text-green-500 border-green-500 rounded-sm px-2 w-full resize-none ${
-        size === "single-line" ? "h-8 overflow-hidden" : "h-24"
-      }`}
-      //   style={{
-      //     WebkitText: censored ? "disc" : "none",
-      //   }}
-      rows={size === "single-line" ? 1 : 3}
-    />
+    <>
+      <div className="flex flex-row border-2 border-green-500 rounded-sm relative">
+        <div className="border-[1px] border-green-500 h-auto text-green-500">
+          i
+        </div>
+        <textarea
+          value={value}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          disabled={disabled}
+          placeholder={placeholder}
+          className={`bg-black text-green-500 px-2 w-full resize-none ${
+            size === "single-line" ? "h-8 overflow-hidden" : "h-24"
+          }`}
+          rows={size === "single-line" ? 1 : 3}
+        />
+      </div>
+    </>
   );
 };
 
